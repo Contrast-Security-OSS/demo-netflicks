bc9795b5efdd2bdfbac092c37327b568cdb858a6
Taylor Mowat <taylormowat@gmail.com>
2025-07-16 13:19:49 +0100

    adding playwright json report for commenting

--- tests/playwright.config.ts
+++ tests/playwright.config.ts
@@ -34,7 +34,7 @@ const config: PlaywrightTestConfig = {
   reporter: [
     ['list', { printSteps: true}],
     ['html', { open: 'never' }],
-    ['json', { outputFile: 'test-results/results.json' }],
+    ['json', { outputFile: 'results.json' }],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
